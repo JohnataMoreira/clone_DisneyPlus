77b7825f801be5ab247fe4fea2a0ceaa3f366af9
JohnataMoreira <165863494+JohnataMoreira@users.noreply.github.com>
2024-09-02 20:20:05 -0300

    configuração do uglify

--- GulpFile.js
+++ GulpFile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 function styles(){
     return gulp.src('./src/styles/main.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
 
@@ -14,7 +21,9 @@ function images(){
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles, images))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
